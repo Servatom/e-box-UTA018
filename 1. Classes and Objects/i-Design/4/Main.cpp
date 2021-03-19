0cf35bc3235009c3c7917268e014872dac660add
Raghav Sharma <raghav_tinker@yahoo.com>
2021-03-20 01:26:58 +0530

    Question 4

---
+++ 1. Classes and Objects/i-Design/4/Main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include "Purchase.cpp"
+
+using namespace std;
+int Purchase::totalCountOfItems = 0;
+float Purchase::totalAmt = 0;
+
+int main(){
+    int number_of_customers;
+    cout<<"Enter the Number of customers :"<<endl;
+    cin>>number_of_customers;
+
+    int net_total = 0;
+    float net_amt = 0;
+    Purchase hist[number_of_customers];
+    for(int i = 0; i < number_of_customers; i++){
+        string userName;
+        int no_items;
+        int purchase_amt;
+        
+        cout<<"Enter the Name of the customer :"<<endl;
+        cin>>userName;
+
+        cout<<"Enter the No of Items purchased :"<<endl;
+        cin>>no_items;
+
+        cout<<"Enter the purchase amount :"<<endl;
+        cin>>purchase_amt;
+
+        hist[i].setUserName(userName);
+        hist[i].setCountOfItems(no_items);
+        hist[i].setamount(purchase_amt);
+        hist[i].settotalCountOfItems(hist[i].getTotalCountOfItems() + no_items);
+        hist[i].setTotalAmt(hist[i].getTotalAmt() + purchase_amt);
+    }
+
+    cout<<"Purchase Details :"<<endl;
+    hist[0].display(hist, number_of_customers);
+    return 0;
+}
---
+++ 1. Classes and Objects/i-Design/4/Purchase.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Purchase{
+    private:
+        string userName;
+        int countOfItems;
+        float amount;
+        float static totalAmt;
+        int static totalCountOfItems;
+    public:
+        string getuserName(){
+            return userName;
+        }
+
+        void setUserName(string a){
+            this->userName = a;
+        }
+
+        int getCountOfItems(){
+            return countOfItems;
+        }
+
+        void setCountOfItems(int b){
+            this->countOfItems = b;
+        }
+
+        float getAmount(){
+            return amount;
+        }
+
+        void setamount(float c){
+            this->amount = c;
+        }
+
+        float getTotalAmt(){
+            return totalAmt;
+        }
+
+        void setTotalAmt(float d){
+            totalAmt = d;
+        }
+
+        int getTotalCountOfItems(){
+            return totalCountOfItems;
+        }
+
+        void settotalCountOfItems(int e){
+            totalCountOfItems = e;
+        }
+
+        void display (Purchase obj[], int n){
+            for(int i = 0; i < n; i++){
+                cout<<"Customer "<<i+1<<" :"<<obj[i].getuserName()<<endl;
+                cout<<"No of Items purchased :"<<obj[i].getCountOfItems()<<endl;
+                cout<<"Purchase amount :"<<obj[i].getAmount()<<endl;
+            }
+
+            cout<<"Total Amount Received :"<<getTotalAmt()<<endl;
+            cout<<"Total Number of Items sold :"<<getTotalCountOfItems()<<endl;
+        }
+};
