cf5b596ef8f1ba204ccb716f718b12e565d9f57e
Raghav Sharma <raghav_tinker@yahoo.com>
2021-03-21 12:01:21 +0530

    Question 1 of idesign

---
+++ 2.Constructor&Destructor/i-Design/Q1/Item.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Item{
+    private:
+        string itemId;
+        string itemName;
+        string itemType;
+        string itemVendor;
+
+    public:
+        string getItemId(){
+            return this->itemId;
+        }
+
+        string getItemName(){
+            return this->itemName;
+        }
+
+        string getItemVendor(){
+            return this->itemVendor;
+        }
+
+        void setItemId(string id){
+            itemId = id;
+        }
+
+        void setItemName(string name){
+            itemName = name;
+        }
+
+        void setItemType(string type){
+            itemType = type;
+        }
+
+        void setitemVendor(string vendor){
+            itemVendor = vendor;
+        }
+
+
+
+        //Constructors
+        Item(){};
+
+        Item(string itemId, string itemName, string itemType, string itemVendor){
+            this->itemId = itemId;
+            this->itemName = itemName;
+            this->itemType = itemType;
+            this->itemVendor = itemVendor;
+        }
+
+        void display(){
+            cout<<"Item details"<<endl;
+            cout<<"Item id: "<<itemId<<endl;
+            cout<<"Item name: "<<itemName<<endl;
+            cout<<"Item type: "<<itemType<<endl;
+            cout<<"Item vendor: "<<itemVendor<<endl;
+        }
+};
---
+++ 2.Constructor&Destructor/i-Design/Q1/Main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include "Item.cpp"
+
+using namespace std;
+
+int main(){
+    int choice;
+    char ch = 'y';
+    cout<<"\nMenu:";
+    cout<<"\n1.Electronics";
+    cout<<"\n2.Others";
+    cout<<"\nEnter your choice:\n";
+    cin>>choice;
+    switch (choice){
+        case 1:
+        {
+            string name, id, type, vendor;
+            type = "Electricals";
+            vendor = "Arun electricals";
+            cout<<"Enter details of the item"<<endl;
+
+            cout<<"Enter item id:"<<endl;
+            cin>>id;
+
+            cout<<"Enter the item name:"<<endl;
+            cin>>name;
+
+            Item item(id, name, type, vendor);
+            item.display();
+
+            break;
+        }
+
+        case 2:
+        {
+            string name, id, type, vendor, t;
+            cout<<"Enter details of the item"<<endl;
+            cout<<"Enter item id:"<<endl;
+            cin>>id;
+            
+            cout<<"Enter the item name:"<<endl; 
+            cin>>name;
+
+            cout<<"Enter the item type:"<<endl;
+            cin>>type;
+
+            cout<<"Enter the item vendor:\n";
+            getline(cin, t);
+            getline(cin, vendor);
+
+            Item item(id, name, type, vendor);
+            item.display();
+
+            break;
+        }
+
+        default:
+            cout<<"Invalid choice"<<endl;
+            break;
+    }
+
+    return 0;
+}
