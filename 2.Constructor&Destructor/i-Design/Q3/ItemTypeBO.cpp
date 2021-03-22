4b89485d82c41df636716272e1ea98e7ef928cc0
Yashvardhan Arora <72307157+yash22arora@users.noreply.github.com>
2021-03-22 13:18:45 +0530

    Updated itemTypeBO

--- 2.Constructor&Destructor/i-Design/Q3/ItemTypeBO.cpp
+++ 2.Constructor&Destructor/i-Design/Q3/ItemTypeBO.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <string>
+#include <cstring>
+#include<iostream>
+#include<string>
 #include "ItemType.cpp"
 
-using namespace std;
-
-class ItemTypeBO{
-    public:
-        ItemType createItem(string itemName, double itemDeposit, double costPerDay){
-            ItemType item_type;
+class ItemTypeBO
+    {
+	public:
+  ItemType createItemType(string itemName,double itemDeposit,double costPerDay)
+	{
+	    ItemType item_type;
             item_type.setCostPerDay(costPerDay);
             item_type.setItemDeposit(itemDeposit);
             item_type.setItemName(itemName);
 
             return item_type;
+	}
+	
+	int searchItemTypeByName(ItemType *itemtype,int n,string searchName)
+	{
+	    int i;
+        for(i=0;i<n;i++)
+        {
+            if(itemtype[i].getItemName()==searchName)
+            return 1;
+            
+            else return 0;
         }
-
-        int searchItemTypeByName(ItemType *itemType, int n, string searchName){
-            for(int i = 0; i < n; i++, itemType++){
-                if(itemType->getItemName() == searchName){
-                    return 1;
-                }
-                else{
-                    continue;
-                }
-            }
-
-            return 0;
-        }
+	}
 };
