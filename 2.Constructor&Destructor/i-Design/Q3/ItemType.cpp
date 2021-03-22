b3cb65d1bfa83062845615b5998f8dabbf9c05e2
Yashvardhan Arora <72307157+yash22arora@users.noreply.github.com>
2021-03-22 13:19:28 +0530

    minor changes

--- 2.Constructor&Destructor/i-Design/Q3/ItemType.cpp
+++ 2.Constructor&Destructor/i-Design/Q3/ItemType.cpp
@@ -1,33 +1,36 @@
-#include <iostream>
-#include <string>
+#include <cstring>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
-class ItemType{
+class ItemType
+{
     private:
-        string itemName;
-        double itemDeposit;
-        double costPerDay;
-    
-    public:
-        string getItemName(){
-            return itemName;
-        }
-
-        double getItemDeposit(){
-            return itemDeposit;
-        }
-
-        double getCostPerDay(){
-            return costPerDay;
-        }
-	    void setItemName(string Name1){
-	        this->itemName=Name1;
-	    }
-	    void setItemDeposit(double Deposit1){
-	        this->itemDeposit=Deposit1;
-	    }
-	    void setCostPerDay(double costperday1){
-  	        this->costPerDay=costperday1;
-	    }
-};
+	string itemName;
+	double itemDeposit,costPerDay;
+	public :
+	string getItemName()
+	{
+	return itemName;
+	}
+	double getItemDeposit()
+	{
+	return itemDeposit;
+	}
+	double getCostPerDay()
+	{
+	return costPerDay;
+	}
+	void setItemName(string Name1)
+	{
+	this->itemName=Name1;
+	}
+	void setItemDeposit(double Deposit1)
+	{
+	this->itemDeposit=Deposit1;
+	}
+	void setCostPerDay(double costperday1)
+	{
+  	this->costPerDay=costperday1;
+	}};
