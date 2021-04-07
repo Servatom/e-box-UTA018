ec258e4d45edbcdf7c09f1efba7ebbcc59883141
Raghav Sharma <raghav_tinker@yahoo.com>
2021-04-08 01:09:07 +0530

    Main.cpp code cleanup
    Also input.txt will have to be uploaded in E-Box

--- 9.File Handling/i-Design/1/Main.cpp
+++ 9.File Handling/i-Design/1/Main.cpp
@@ -2,22 +2,22 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
 int main(){
- string fname;
- string line;
- cout<<"Give filename:"<<endl;
- cin>>fname;
- ifstream ifile;
- ifile.open(fname);
- if(ifile.fail())
- {
- cout<<endl<<"Error! Incorrect file.";
- }
- else
- {
- while(getline(ifile, line))
- {
- cout<<line<<endl;
- }
- }
+    string fname;
+    string line;
+    cout<<"Give filename:"<<endl;
+    cin>>fname;
+    
+    ifstream ifile;
+    ifile.open(fname);
+    if(ifile.fail()){
+        cout<<endl<<"Error! Incorrect file.";
+    }
+    else{
+        while(getline(ifile, line)){
+            cout<<line<<endl;
+        }
+    }
+    return 0;
 }
