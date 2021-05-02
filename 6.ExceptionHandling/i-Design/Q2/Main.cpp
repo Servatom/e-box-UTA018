e3de2b0e7697c658393ada1d759f3efa5482048b
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2021-05-02 14:05:57 +0530

    Exception Handling Q2 Done

---
+++ 6.ExceptionHandling/i-Design/Q2/Main.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stdio.h>
+#include<exception>
+#include<cstring>
+#include<ctype.h>
+#include<bits/stdc++.h>
+#include "Itemtype.cpp"
+using namespace std;
+int main()
+{
+        string name;
+        double deposit;
+        double costPerday;
+
+        Itemtype it;
+
+        cout<<"Enter the itemtype name: "<<endl;
+        getline(cin,name);
+        it.setName(name);
+
+        cout<<"Enter item deposit:"<<endl;
+        cin>>deposit;
+        it.setDeposit(deposit);
+
+        try
+        {
+
+        if(deposit=int(deposit) )
+
+             { cout<<"Enter item cost per day: "<<endl;
+            cin>>costPerday;
+             it.setCostPerday(costPerday);
+                 if(costPerday=int(costPerday))
+                {
+
+                 it.display();
+                }
+
+             else
+             throw 1;
+             }
+
+        else
+
+       throw 1;
+
+        }
+        catch(int )
+        {
+
+            cout<<"NumberFormatException: "<<"Price should always be numeric "<<endl;
+        }
+
+
+
+        return 0;
+}
