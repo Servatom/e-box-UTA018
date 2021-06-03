d31795b1ebeeb3dae001e7b69b1e79ba2bd32732
Raghav Sharma <raghav_tinker@yahoo.com>
2021-06-03 12:34:44 +0530

    decrement increment added

--- Notes/Operator Overloading/ArithmeticOperatorOverloading.cpp
+++ Notes/Operator Overloading/ArithmeticOperatorOverloading.cpp
@@ -42,6 +42,54 @@ class MinusOverload{
         }
 };
 
+class IncrementOverload{
+    private:
+        int number1;
+    public:
+        IncrementOverload(){}
+        IncrementOverload(int number1){
+            this->number1 = number1;
+        }
+
+        void operator++(){ //For pre increment
+            cout<<"pre"<<endl;
+            ++this->number1;
+        }
+
+        void operator++(int x){ //This is for post increment
+            cout<<"post"<<endl;
+            this->number1++;
+        }
+
+        void display(){
+            cout<<number1<<endl;
+        }
+};
+
+class DecrementOverload{
+    private:
+        int number1;
+    public:
+        DecrementOverload(){}
+        DecrementOverload(int number1){
+            this->number1 = number1;
+        }
+
+        void operator--(){ //For pre increment
+            cout<<"pre"<<endl;
+            --this->number1;
+        }
+
+        void operator--(int x){ //This is for post increment
+            cout<<"post"<<endl;
+            this->number1--;
+        }
+
+        void display(){
+            cout<<number1<<endl;
+        }
+};
+
 
 int main(){
     //+ operator overload
@@ -57,5 +105,18 @@ int main(){
     obj6.showMe();
 
     //Similarly *, / can be overloaded
+
+    //++ increment
+    IncrementOverload obj7(12);
+    obj7++; // output => post
+    ++obj7; //output => pre
+    obj7.display(); //Output => 14
+
+    //-- decrement is similar
+    DecrementOverload obj8(12);
+    obj8--; // output => post
+    --obj8; //output => pre
+    obj8.display(); //Output => 14
+    
     return 0;
 }
